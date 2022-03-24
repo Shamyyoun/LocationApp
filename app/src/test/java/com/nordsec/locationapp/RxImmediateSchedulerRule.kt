412cb57a87139a310aac58a3d7b51633e45fa11e
Mahmoud Elshamy <shamyyoun@gmail.com>
2022-03-24 20:26:05 +0400

    Refactored LocationsViewModelTest and added more test cases covering getLocationsSortedByDistance()

---
+++ app/src/test/java/com/nordsec/locationapp/RxImmediateSchedulerRule.kt
@@ -0,0 +1,40 @@
+package com.nordsec.locationapp
+
+import io.reactivex.rxjava3.android.plugins.RxAndroidPlugins
+import io.reactivex.rxjava3.core.Scheduler
+import io.reactivex.rxjava3.disposables.Disposable
+import io.reactivex.rxjava3.internal.schedulers.ExecutorScheduler
+import io.reactivex.rxjava3.plugins.RxJavaPlugins
+import org.junit.rules.TestRule
+import org.junit.runner.Description
+import org.junit.runners.model.Statement
+import java.util.concurrent.TimeUnit
+
+class RxImmediateSchedulerRule : TestRule {
+    private val immediate = object : Scheduler() {
+        override fun scheduleDirect(run: Runnable, delay: Long, unit: TimeUnit): Disposable {
+            return super.scheduleDirect(run, 0, unit)
+        }
+
+        override fun createWorker(): Worker {
+            return ExecutorScheduler.ExecutorWorker(Runnable::run, true, true)
+        }
+    }
+
+    override fun apply(base: Statement?, description: Description?) = object : Statement() {
+        override fun evaluate() {
+            RxJavaPlugins.setInitIoSchedulerHandler { immediate }
+            RxJavaPlugins.setInitComputationSchedulerHandler { immediate }
+            RxJavaPlugins.setInitNewThreadSchedulerHandler { immediate }
+            RxJavaPlugins.setInitSingleSchedulerHandler { immediate }
+            RxAndroidPlugins.setInitMainThreadSchedulerHandler { immediate }
+
+            try {
+                base?.evaluate()
+            } finally {
+                RxJavaPlugins.reset()
+                RxAndroidPlugins.reset()
+            }
+        }
+    }
+}
--- app/src/test/java/com/nordsec/locationapp/presentation/locations/LocationsViewModelTest.kt
+++ app/src/test/java/com/nordsec/locationapp/presentation/locations/LocationsViewModelTest.kt
@@ -1,54 +1,53 @@
 package com.nordsec.locationapp.presentation.locations
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.nordsec.locationapp.RxImmediateSchedulerRule
 import com.nordsec.locationapp.domain.models.Location
 import com.nordsec.locationapp.domain.useCases.GetLocationsSortedByCityNameUseCase
 import com.nordsec.locationapp.domain.useCases.GetLocationsSortedByDistanceUseCase
 import com.nordsec.locationapp.getOrAwaitValue
-import io.reactivex.rxjava3.android.plugins.RxAndroidPlugins
 import io.reactivex.rxjava3.core.Single
-import io.reactivex.rxjava3.plugins.RxJavaPlugins
-import io.reactivex.rxjava3.schedulers.TestScheduler
 import org.junit.Assert.assertEquals
 import org.junit.Assert.assertTrue
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
+import org.mockito.kotlin.any
 import org.powermock.api.mockito.PowerMockito
 import java.io.IOException
-import java.util.concurrent.TimeUnit
 
 
 class LocationsViewModelTest {
     @get:Rule
+    val rxImmediateRule = RxImmediateSchedulerRule()
+
+    @get:Rule
     val instantExecutorRule = InstantTaskExecutorRule()
 
-    private val useCases = LocationsUseCases(
-        getLocationsSortedByCityNameUseCase = PowerMockito.mock(GetLocationsSortedByCityNameUseCase::class.java),
-        getLocationsSortedByDistanceUseCase = PowerMockito.mock(GetLocationsSortedByDistanceUseCase::class.java)
-    )
-    private val viewModel = LocationsViewModel(useCases)
-    private val testScheduler = TestScheduler()
+    private lateinit var useCases: LocationsUseCases
+    private lateinit var viewModel: LocationsViewModel
+
+    private lateinit var testOriginLocation: Location
 
     @Before
     fun setUp() {
-        RxJavaPlugins.setInitIoSchedulerHandler { testScheduler }
-        RxAndroidPlugins.setInitMainThreadSchedulerHandler { testScheduler }
-    }
+        // Create use cases and the view model
+        useCases = LocationsUseCases(
+            getLocationsSortedByCityNameUseCase = PowerMockito.mock(
+                GetLocationsSortedByCityNameUseCase::class.java
+            ),
+            getLocationsSortedByDistanceUseCase = PowerMockito.mock(
+                GetLocationsSortedByDistanceUseCase::class.java
+            )
+        )
+        viewModel = LocationsViewModel(useCases)
 
-    @Test
-    fun `getLocationsSortedByCityName() is emitting loading state when subscribing to the useCase observable`() {
-        // Given
-        val testObservable = Single.just(emptyList<Location>())
-        PowerMockito.doReturn(testObservable)
-            .`when`(useCases.getLocationsSortedByCityNameUseCase)
-            .invoke()
-
-        // When
-        viewModel.getLocationsSortedByCityName()
-
-        // Then
-        assertEquals(LocationsViewState.Loading, viewModel.viewState.getOrAwaitValue())
+        // Create test origin
+        testOriginLocation = Location(
+            city = "Cairo",
+            latitude = 0.155f,
+            longitude = 0.166f
+        )
     }
 
     @Test
@@ -73,7 +72,6 @@ class LocationsViewModelTest {
 
         // When
         viewModel.getLocationsSortedByCityName()
-        testScheduler.advanceTimeBy(1, TimeUnit.SECONDS) // Move 1 second
 
         // Then
         // Assert that current view state is Locations
@@ -101,7 +99,76 @@ class LocationsViewModelTest {
 
         // When
         viewModel.getLocationsSortedByCityName()
-        testScheduler.advanceTimeBy(1, TimeUnit.SECONDS) // Move 1 second
+
+        // Then
+        // Assert that current view state is Error
+        assertTrue(viewModel.viewState.getOrAwaitValue() is LocationsViewState.Error)
+
+        val errorState = viewModel.viewState.getOrAwaitValue() as LocationsViewState.Error
+
+        // Assert that error message is correct
+        assertEquals(testException.message, errorState.errorMsg)
+    }
+
+    @Test
+    fun `getLocationsSortedByDistance() is emitting locations state when the useCase emits valid locations`() {
+        // Given
+        val testLocations = listOf(
+            Location(
+                city = "Dubai",
+                latitude = 0.111f,
+                longitude = 0.122f
+            ),
+            Location(
+                city = "Berlin",
+                latitude = 0.133f,
+                longitude = 0.144f
+            ),
+            Location(
+                city = "Cairo",
+                latitude = 0.155f,
+                longitude = 0.166f
+            )
+        )
+        val testObservable = Single.just(testLocations)
+        PowerMockito.doReturn(testObservable)
+            .`when`(useCases.getLocationsSortedByDistanceUseCase)
+            .invoke(any())
+
+        // When
+        viewModel.getLocationsSortedByDistance(testOriginLocation)
+
+        // Then
+        // Assert that current view state is Locations
+        assertTrue(viewModel.viewState.getOrAwaitValue() is LocationsViewState.Locations)
+
+        val locationsState = viewModel.viewState.getOrAwaitValue() as LocationsViewState.Locations
+
+        // Assert that emitted locations list size is the same as the test one
+        assertEquals(testLocations.size, locationsState.locations.size)
+        assertEquals(testLocations[0], locationsState.locations[0])
+        assertEquals(testLocations[1], locationsState.locations[1])
+        assertEquals(testLocations[2], locationsState.locations[2])
+
+        // Assert that sort by in the state is by distance from city
+        assertTrue(locationsState.sortBy is LocationsSortBy.DistanceFromCity)
+        assertEquals(
+            testOriginLocation,
+            (locationsState.sortBy as LocationsSortBy.DistanceFromCity).location
+        )
+    }
+
+    @Test
+    fun `getLocationsSortedByDistance() is emitting error state when the useCase emits error`() {
+        // Given
+        val testException = IOException("some error")
+        val testObservable = Single.error<List<Location>>(testException)
+        PowerMockito.doReturn(testObservable)
+            .`when`(useCases.getLocationsSortedByDistanceUseCase)
+            .invoke(any())
+
+        // When
+        viewModel.getLocationsSortedByDistance(testOriginLocation)
 
         // Then
         // Assert that current view state is Error
